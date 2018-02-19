d334c6f1ba780745f1e7a2109a8506b33f5f85fa
Sunita Kunwar <sunitakwr21@gmail.com>
2018-02-19 11:42:39 +0545

    q5

---
+++ question5.py
@@ -0,0 +1,10 @@
+'''Define a class named Nepali which has a static method called printNationality.
+Which when invoked prints "Nepalese"'''
+
+class Nepali:
+ @staticmethod
+ def printNationality():
+  print("Nepalese")
+
+
+Nepali.printNationality()
