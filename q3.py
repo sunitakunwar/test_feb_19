021236275d0de33f1499fc426640812427c705f5
sunita kunwar <sunitakwr21@gmail.com>
2018-02-19 11:29:35 +0545

    Delete q3.py

--- q3.py
+++
@@ -1,5 +0,0 @@
-'''Create a Student class and initialize it with name and roll number. Make methods to :
-      a. Display - It should display all informations of the student.
-      b. setAge - It should assign age to student
-      c. setMarks - It should assign marks to the student. '''
-
