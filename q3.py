a2a85243cbcd9edea356cb6e4927f1ff54df51df
Sunita Kunwar <sunitakwr21@gmail.com>
2018-02-19 10:56:43 +0545

    q2

---
+++ q3.py
@@ -0,0 +1,5 @@
+'''Create a Student class and initialize it with name and roll number. Make methods to :
+      a. Display - It should display all informations of the student.
+      b. setAge - It should assign age to student
+      c. setMarks - It should assign marks to the student. '''
+
